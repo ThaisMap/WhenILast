74d171a6e06446d47004677201ae647088102445
Thais Maria <thais.map@hotmail.com>
2020-08-17 21:01:18 -0300

    Projeto criado
    Página inicial limpa

---
+++ WhenDidILast/App.js
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import { View, Text } from 'react-native';
+
+
+export default function App(){
+  return(
+    <View>
+      <Text>This is the new most amazing thing</Text>
+    </View>
+  );
+}
