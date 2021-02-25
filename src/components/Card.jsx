2775d8ac1ee17a72f186d5b6c70240c7a23b6334
Thais <thais.map@hotmail.com>
2021-02-25 17:58:17 -0300

    Cards editáveis e com estado próprio

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,105 @@
 import styles from "../styles/Card.module.css";
-import { FaCoffee } from 'react-icons/fa'
+import { FaCoffee, FaPen, FaCheck } from "react-icons/fa";
+import { useState } from "react";
 
-export default function Card(){
-     return (
-        <div className={styles.container}>
-            <article className={styles.card}>
-                <FaCoffee size={36} color={'#006e25'} />
-                <div>
-                    <p>Tomei café puro</p>
-                    <p><span>10/02/2021</span> - <span>14 dias</span> atrás</p>
-                    <p>Estava com sono e não tinha maçã</p>
-                </div>
-            </article>
-        </div>
-     )
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
+
+export default function Card() {
+  const [isEditing, setIsEditing] = useState(false);
+  const [activity, setActivity] = useState({
+    title: "Tomei café puro",
+    date: new Date("2021-02-16 00:00"),
+    comment: "Estava com sono e não tinha maçã",
+  });
+
+  function elapsedTime() {
+    const msInDay = 1000 * 60 * 60 * 24;
+    const today = new Date();
+    const actDate = new Date(activity.date);
+    const elapsed = today - actDate;
+
+    if (elapsed < 0) return "Viajou no tempo";
+
+    if (elapsed > msInDay) {
+      const days = Math.floor(elapsed / msInDay);
+      return days == 1 ? `${days} dia atrás` : `${days} dias atrás`;
+    } else return "Hoje mesmo";
+  }
+
+  function handleEditingButton() {
+    setIsEditing(!isEditing);
+  }
+
+  function handleDateChange(selected) {
+    setActivity({
+      ...activity,
+      date: selected,
+    });
+  }
+
+  function handleCommentChange(event) {
+    setActivity({
+      ...activity,
+      comment: event.target.value,
+    });
+  }
+
+  function handleTitleChange(event) {
+    setActivity({
+      ...activity,
+      title: event.target.value,
+    });
+  }
+
+  return (
+    <div className={styles.container}>
+      {isEditing ? (
+        <form className={styles.card}>
+          <div className={styles.icon}>
+            <FaCoffee size={"3rem"} color={"#006e25"} />
+          </div>
+
+          <div className={styles.content}>
+            <input
+              type="text"
+              name="title"
+              id="title"
+              onChange={handleTitleChange}
+              defaultValue={activity.title}
+            />
+            <DatePicker selected={activity.date} onChange={handleDateChange} />
+            <input
+              type="text"
+              name="comment"
+              id="comment"
+              defaultValue={activity.comment}
+              onChange={handleCommentChange}
+            />
+          </div>
+          <button className={styles.editButton} onClick={handleEditingButton}>
+            <FaCheck size={"1em"} color={"#006e25"} />
+          </button>
+        </form>
+      ) : (
+        <article className={styles.card}>
+          <div className={styles.icon}>
+            <FaCoffee size={"3rem"} color={"#006e25"} />
+          </div>
+          <div className={styles.content}>
+            <p className={styles.title}>{activity.title}</p>
+            <p className={styles.date}>
+              <span> {new Date(activity.date).toLocaleDateString()}</span> -{" "}
+              <span>{elapsedTime()}</span>{" "}
+            </p>
+            <p className={styles.comment}>{activity.comment}</p>
+          </div>
+          <button className={styles.editButton} onClick={handleEditingButton}>
+            <FaPen size={"1em"} color={"#006e25"} />
+          </button>
+        </article>
+      )}
+    </div>
+  );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
 import styles from "../styles/Header.module.css";
-
+import { FaSearch } from 'react-icons/fa'
 
 export default function Header(){
      return (
-        <header>
-         <h1 className={styles.title}>WDIL</h1>
+        <header className={styles.container}>           
+           <img src="images/sided_logo.svg" className={styles.title} alt="Quando foi que eu fiz isso?"/> 
+         <div className={styles.searchBox}>
+            <input type="text" name="search" placeholder="Pesquisar..."  id="search"/>
+            <button> <FaSearch size={20} /> </button>
+         </div>
       </header>
      )
 }
